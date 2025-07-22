c9feef6f427da6293a556f2db2512cd81f2c5cf2
sbx_user1051 <sbx_user1051@169.254.19.161>
2025-07-22 19:55:47 +0000

    Tue Jul 22 19:55:47 UTC 2025

--- test/solution_tests/CHK/test_checkout_solution.py
+++ test/solution_tests/CHK/test_checkout_solution.py
@@ -1,9 +1,14 @@
-from solutions.CHK.checkout_solutions import CheckoutSolution
+from solutions.CHK.checkout_solution import CheckoutSolution
 
 import pytest
 
 class TestCheckout:
 
-    @pytest.mark.parametrize("")
-    def test_checkout__one_item(self):
-        pass
+    @pytest.mark.parametrize("sku,price", [
+        ("A", 50),
+        ("B", 30),
+        ("C", 20),
+        ("D", 15),
+    ])
+    def test_checkout__one_item(self, sku, price):
+        assert CheckoutSolution().checkout(sku) == price
