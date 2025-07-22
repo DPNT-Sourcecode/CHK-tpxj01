e4c69e5c466260c710dc52cc2cc9821c4de6f423
sbx_user1051 <sbx_user1051@169.254.19.161>
2025-07-22 20:19:47 +0000

    Tue Jul 22 20:19:47 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,38 +4,42 @@ class Item:
         self.price = price
 
 
-class Inventory:
+class Catalogue:
     def __init__(self):
         self.items = {}
 
     def add_item(self, item: Item):
         self.items[item.sku] = item
 
+class Offer:
+    def __init__(self, item: Item, quantity: int, price: int):
+        self.item = item
+        self.quantity = quantity
+        self.price = price
+
 class CheckoutSolution:
 
-    # Not needed just yet, thinking ahead a bit
-    # class Offer:
-    #     def __init__(self, item: CheckoutSolution.Item, quantity: int, price: int):
-    #         self.item = item
-    #         self.quantity = quantity
-    #         self.price = price
-
     def __init__(self):
-        # Build our product inventory
-        self.inventory = Inventory()
-        self.inventory.add_item(Item("A", 50))
-        self.inventory.add_item(Item("B", 30))
-        self.inventory.add_item(Item("C", 20))
-        self.inventory.add_item(Item("D", 15))
+        # Build our product catalogue
+        self.catalogue = Catalogue()
+        self.catalogue.add_item(Item("A", 50))
+        self.catalogue.add_item(Item("B", 30))
+        self.catalogue.add_item(Item("C", 20))
+        self.catalogue.add_item(Item("D", 15))
 
 
     # skus = unicode string
     def checkout(self, skus):
         # skus is "a string containing the SKUs of all the products in the basket"
         # Space separated? Comma separated?
+        # Gonna assume they're not for now
 
         # "Sometimes, the requirements are unclear. Getting feedback from your users may be the key to moving
         # forward."
         # So to get feedback from "users", I need to "deploy to production"?
 
-        return self.inventory.items[skus].price
+        total = 0
+        for sku in skus:
+            total += self.catalogue.items[sku].price
+        return total
+
