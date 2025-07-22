056d05f3b0c0777071fb4b38bf82ecf8338fc9bf
sbx_user1051 <sbx_user1051@169.254.19.161>
2025-07-22 19:58:47 +0000

    Tue Jul 22 19:58:47 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,18 @@
 
 class CheckoutSolution:
 
+    class Item:
+        def __init__(self, price: int):
+            self.price = price
+
     # skus = unicode string
     def checkout(self, skus):
+        # skus is "a string containing the SKUs of all the products in the basket"
+        # Space separated? Comma separated?
+
+        # "Sometimes, the requirements are unclear. Getting feedback from your users may be the key to moving
+        # forward."
+        # 
+
         raise NotImplementedError()
+
