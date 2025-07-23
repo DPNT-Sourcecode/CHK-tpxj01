4db12993dd2a218ea6776f34acc749030ac7844b
sbx_user1051 <sbx_user1051@169.254.19.161>
2025-07-23 05:54:58 +0000

    Wed Jul 23 05:54:58 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -41,14 +41,6 @@ class CheckoutSolution:
 
     # skus = unicode string
     def checkout(self, skus):
-        # skus is "a string containing the SKUs of all the products in the basket"
-        # Space separated? Comma separated?
-        # Gonna assume they're not for now
-
-        # "Sometimes, the requirements are unclear. Getting feedback from your users may be the key to moving
-        # forward."
-        # So to get feedback from "users", I need to "deploy to production"?
-
         # Map of sku to quantity
         basket = {}
 
